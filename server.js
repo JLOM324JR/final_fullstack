44dbac374128e5fb30bc3fefebcde33c35ebaed4
JR <radtanaporn.kn@gmail.com>
2018-12-15 14:28:57 +0700

    test_api

--- server.js
+++ server.js
@@ -15,6 +15,24 @@ extended: true
 app.get('/', function (req, res) {
     res.send('Express is running');
 });
+var output = {
+    status: 'success',
+    message: 'REST API is working'
+}
+/*
+app.get('/api/json', function (req, res) {
+    res.status(500).json(output);
+});
+*/
+
+app.get('/api/categories', db.getcategories)
+
+
+
+
+
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
 console.log('App is running on http://localhost:' + port);
